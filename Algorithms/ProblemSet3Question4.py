6a6fcdcd446cab8c9d30d01a6c3b4fa2bdc144a5
Shuaishuai Li <shuaishuai.li@uni-ulm.de>
2018-09-03 23:32:46 +0200

    Update ProblemSet3Question4.py

--- Algorithms/ProblemSet3Question4.py
+++ Algorithms/ProblemSet3Question4.py
@@ -1,6 +1,6 @@
 
 '''
-Consider a group of kk people. Assume that each person's birthday is drawn uniformly 
+Consider a group of k people. Assume that each person's birthday is drawn uniformly 
 at random from the 365 possibilities. (And ignore leap years.) What is the smallest 
 value of kk such that the expected number of pairs of distinct people with the same 
 birthday is at least one?
